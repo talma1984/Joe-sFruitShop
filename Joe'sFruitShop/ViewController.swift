5e9ec7d919882f11664a856fd41c856f9788e18d
tal <talmarom1984@gmail.com>
2022-02-17 09:49:18 +0200

    fruit shop first commit

---
+++ Joe'sFruitShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joe'sFruitShop
+//
+//  Created by Mac on 17/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

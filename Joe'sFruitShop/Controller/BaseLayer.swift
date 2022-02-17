6f1a4ff9a71e751bfb01b52e72b96edfd055c444
tal <talmarom1984@gmail.com>
2022-02-17 13:13:13 +0200

    set background image

--- Joe'sFruitShop/Controller/BaseLayer.swift
+++ Joe'sFruitShop/Controller/BaseLayer.swift
@@ -12,10 +12,14 @@ private let backGroundImage = UIImageView()
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+    //set the background image of the main screen
     func backGround(){
         backGroundImage.image = UIImage(named: "back")
         backGroundImage.translatesAutoresizingMaskIntoConstraints = false
-        
+        view.addSubview(backGroundImage)
+        backGroundImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backGroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        backGroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        view.sendSubviewToBack(backGroundImage)
     }
 }
